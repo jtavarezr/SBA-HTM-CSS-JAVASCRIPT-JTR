e37d4b6ec5a26bedfc5dd31242a168832c4650d5
Jonathan Tavarez <jtavarez@tjss.us>
2024-06-27 09:45:45 -0400

    Adding DockerFile and fix some bugs in todolist

---
+++ src/main/java/com/jtavarez/sbahtmcssjavascriptjtr/SbaHtmCssJavascriptJtrApplication.java
@@ -0,0 +1,13 @@
+package com.jtavarez.sbahtmcssjavascriptjtr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbaHtmCssJavascriptJtrApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbaHtmCssJavascriptJtrApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/jtavarez/sbahtmcssjavascriptjtr/SbaHtmCssJavascriptJtrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtavarez.sbahtmcssjavascriptjtr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbaHtmCssJavascriptJtrApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
